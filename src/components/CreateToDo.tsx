5e77adce10539a6eb07ce7dca27cb979fbef21bb
Seio924 <134473627+Seio924@users.noreply.github.com>
2023-08-23 00:00:42 +0900

    select

---
+++ src/components/CreateToDo.tsx
@@ -0,0 +1,36 @@
+import { useForm } from "react-hook-form";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { categoryState, toDoState } from "../atoms";
+
+export interface Iform {
+  toDo: string;
+}
+
+function CreateToDo() {
+  const setToDos = useSetRecoilState(toDoState);
+  const category = useRecoilValue(categoryState);
+  const { register, handleSubmit, setValue } = useForm<Iform>();
+
+  const handleValid = ({ toDo }: Iform) => {
+    console.log("add to do", toDo);
+    setToDos((oldToDos) => [
+      { text: toDo, id: Date.now(), category },
+      ...oldToDos,
+    ]);
+    setValue("toDo", "");
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleValid)}>
+      <input
+        {...register("toDo", {
+          required: "Please write a To Do",
+        })}
+        placeholder="Write a to do"
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+export default CreateToDo;
